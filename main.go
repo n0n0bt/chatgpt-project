896e7d94e3b654e0350aedc30e3b72c6df54cd44
Naumche <nj@skalar-design.de>
2024-04-15 14:49:26 +0200

    test

--- main.go
+++ main.go
@@ -4,16 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
-var APIkey = ""
-var TheClient = CreateClient(APIkey, "https://api.openai.com/v1/completions")
+var APIkey = os.Getenv("CHATGPT_API_KEY")
+var Client = gpt.CreateClient(APIkey, "https://api.openai.com/v1/completions")
 
 func main() {
 	fmt.Println("API Key:", APIkey)
-	answ, err := TheClient.AskGPTansw("what is a ferret")
+	answ, err := Client.AskGPTansw("what is a ferret")
 	fmt.Println("answ:", answ)
 	fmt.Println("err:", err)
 
@@ -37,7 +38,7 @@ func askGEThandler(c *gin.Context) {
 // askPostHandler asks chatGPT question from user and serves response
 func askPOSThandler(c *gin.Context) {
 	question := c.PostForm("question")
-	answ, err := TheClient.AskGPTansw(question)
+	answ, err := Client.AskGPTansw(question)
 	if err != nil {
 		fmt.Println("error asking Chat GPT:", err)
 		c.HTML(http.StatusInternalServerError, "index.html", gin.H{"message": "sorry, something went wrong"})
